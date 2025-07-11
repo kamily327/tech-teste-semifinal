c3a49dd61e1bb4e108cb822921c6f26eabf56afe
kamily_dev <matheusavakin844@gmail.com>
2025-07-10 23:44:04 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,334 @@
+const smartphones = [
+  {
+    imagem: "./images/phones/samsung-s24.webp",
+    alt: "Samsung Galaxy S24 Ultra 256GB 12GB RAM",
+    desconto: "-10%",
+    titulo: "Samsung Galaxy S24 Ultra",
+    memoria: "256GB 12GB RAM",
+    precoParcelado: {
+    valorTotal: 5999,
+    parcelas: 10,
+    valorParcela: 599.9,
+    descricao: "sem juros",
+    },
+    precoPix: 5399,
+  },
+
+  {
+    imagem: "./images/phones/samsung-s24-fe.webp",
+    alt: "Samsung Galaxy S24 Ultra FE 128GB 8GB RAM",
+    desconto: "-10%",
+    titulo: "Samsung Galaxy S24 Ultra FE",
+    memoria: "128GB 8GB RAM",
+    precoParcelado: {
+    valorTotal: 2887,
+    parcelas: 10,
+    valorParcela: 288.78,
+    descricao: "sem juros",
+    },
+    precoPix: 2599,
+  },
+
+  {
+    imagem: "./images/phones/samsung-a56.webp",
+    alt: "Samsung Galaxy A56 128GB 8GB RAM",
+    desconto: "-10%",
+    titulo: "Samsung Galaxy A56",
+    memoria: "128GB 8GB RAM",
+    precoParcelado: {
+    valorTotal: 2099,
+    parcelas: 10,
+    valorParcela: 209,
+    descricao: "sem juros",
+    },
+    precoPix: 1899,
+  },
+
+  {
+    imagem: "./images/phones/iphone-14.webp",
+    alt: "iPhone 14 128GB 4GB RAM",
+    desconto: "-10%",
+    titulo: "iPhone 14", 
+    memoria: "128GB 8GB RAM",
+    precoParcelado: {
+    valorTotal: 4199,
+    parcelas: 10,
+    valorParcela: 419.90,
+    descricao: "sem juros",
+    },
+    precoPix: 3799,
+  },
+  {
+    imagem: "./images/phones/iphone-16-preto.webp",
+    alt: "iPhone 16 128GB 8GB RAM",
+    desconto: "-10%",
+    titulo: "iPhone 16",
+    memoria: "128GB 8GB RAM",
+    precoParcelado: {
+    valorTotal: 5499,
+    parcelas: 10,
+    valorParcela: 549.90,
+    descricao: "sem juros",
+    },
+    precoPix: 2599,
+  },
+ {
+     imagem:'./images/phones/iphone-16e.webp',
+     alt: 'iPhone 16E 256GB 8GB RAM',
+     desconto: '-10%',
+     titulo: 'iPhone 16E',
+     memoria: '256GB 8GB RAM',
+     precoParcelado:{
+     valorTotal:5832,
+     parcelas:10,
+     valorParcela: 583.23,
+     descricao: 'sem juros',
+     },
+     precoPix:5248
+    },
+
+
+    {
+     imagem:'./images/phones/motorola-g75.webp',
+     alt: 'Motorola Moto G75 256GB 8GB RAM',
+     desconto: '-10%',
+     titulo: 'Motorola Moto G75',
+     memoria: '256GB 8GB RAM',
+     precoParcelado:{
+     valorTotal:1849,
+     parcelas:10,
+     valorParcela: 184.90,
+     descricao: 'sem juros',
+     },
+     precoPix: 1664
+    },
+
+      {
+     imagem:'./images/phones/motorola-edge-neo.webp',
+     alt: 'Motorola Edge 50 Neo 512GB 24GB RAM',
+     desconto: '-10%',
+     titulo: 'Motorola Edge 50 Neo',
+     memoria: '512GB 24GB RAM',
+     precoParcelado:{
+       valorTotal: 4298,
+       parcelas:10,
+       valorParcela: 209,
+       descricao: 'sem juros',
+     },
+     precoPix: 3997
+      },
+];
+// TABLETS
+
+const tablets = [
+
+    {
+    imagem:'./images/tablets/tab-a9.webp',
+    alt:'Tablet Samsung Galaxy Tab A9  ',
+    desconto:'-10%',
+    titulo:'Tablet Samsung Galaxy Tab A9 128GB 4GB RAM',
+    memoria:'128GB 4GB RAM',
+    precoParcelado:{
+    valorTotal: 1599,
+    parcelas: 10,
+    valorParcela: 159,
+    descricao:'sem juros'
+    },
+    precoPix: 1899
+  },
+
+
+   {
+    imagem:'./images/tablets/tab-s10-fe.webp',
+    alt:'Tablet Galaxy Tab S10 Ultra  ',
+    desconto:'-10%',
+    titulo:'Tablet Galaxy Tab S10 Ultra 512GB 12GB RAM',
+    memoria:'512GB 12GB RAM',
+    precoParcelado:{
+    valorTotal: 7.899,
+    parcelas: 10,
+    valorParcela: 789,
+    descricao:'sem juros'
+    },
+    precoPix: 6899
+  },
+
+ 
+
+
+
+
+
+  
+
+
+   {
+    imagem:'./images/tablets/tab-s6-lite.webp',
+    alt:'Tablet Samsung Galaxy Tab S6 Lite 64GB 4GB RAM',
+    desconto:'-10%',
+    titulo:'Tablet Samsung Galaxy Tab S6 Lite',
+    memoria:'64GB 4GB RAM',
+    precoParcelado:{
+    valorTotal: 2179,
+    parcelas: 10,
+    valorParcela: 217,
+    descricao:'sem juros'
+    },
+    precoPix: 1961
+  },
+
+
+  
+   {
+    imagem:'./images/tablets/tab-s6-lite.webp',
+    alt:'Tablet Samsung Galaxy Tab S6 Lite 64GB 4GB RAM',
+    desconto:'-10%',
+    titulo:'Tablet Samsung Galaxy Tab S6 Lite',
+    memoria:'64GB 4GB RAM',
+    precoParcelado:{
+    valorTotal: 2179,
+    parcelas: 10,
+    valorParcela: 217,
+    descricao:'sem juros'
+    },
+    precoPix: 1961
+  },
+
+    {
+    imagem:'./images/tablets/tab-s10-fe.webp',
+    alt:'Tablet Galaxy Tab S10 Ultra  ',
+    desconto:'-10%',
+    titulo:'Tablet Galaxy Tab S10 Ultra 512GB 12GB RAM',
+    memoria:'512GB 12GB RAM',
+    precoParcelado:{
+    valorTotal: 7.899,
+    parcelas: 10,
+    valorParcela: 789,
+    descricao:'sem juros'
+    },
+    precoPix: 6899
+  },
+
+  {
+    imagem:'./images/tablets/tab-s6-lite.webp',
+    alt:'Tablet Samsung Galaxy Tab S6 Lite 64GB 4GB RAM',
+    desconto:'-10%',
+    titulo:'Tablet Samsung Galaxy Tab S6 Lite',
+    memoria:'64GB 4GB RAM',
+    precoParcelado:{
+    valorTotal: 2179,
+    parcelas: 10,
+    valorParcela: 217,
+    descricao:'sem juros'
+    },
+    precoPix: 1961
+  },
+   {
+    imagem:'./images/tablets/tab-s10-fe.webp',
+    alt:'Tablet Galaxy Tab S10 Ultra  ',
+    desconto:'Super promoção',
+    titulo:'Tablet Galaxy Tab S10 Ultra 512GB 12GB RAM',
+    memoria:'512GB 12GB RAM',
+    precoParcelado:{
+    valorTotal: 7.899,
+    parcelas: 10,
+    valorParcela: 789,
+    descricao:'sem juros'
+    },
+    precoPix: 6899
+  },
+
+]
+
+function formatarPreco(preco){
+   return preco.toFixed(2).replace('.', ',')
+}
+const celularesContainer = document.getElementById('celulares');
+smartphones.forEach((p) => {
+  celularesContainer.innerHTML +=`
+  <div class="card flex text-center">
+  <div class="img-content relative">
+      <div class="img w-[270px]">
+       <img src="${p.imagem}" alt="${p.alt}"/>
+      </div>
+
+      <div class="desconto absolute top-[0]">
+       <span class="bg-[#cc4410] text-white rounded-lg py-[3px] px-[5px] ml-[2px] font-bold text-[0.9rem]">
+        ${p.desconto}
+       </span>
+      </div>
+
+     <div class="title font-bold ">
+      <h1 class="mt-[5px]">${p.titulo}</h1>
+     </div>
+
+    <div class="memoria">
+     <p class="text-[1rem]">${p.memoria}</p>
+   </div>
+
+ <div class="price">  
+    <span class="text-[0.8rem] text-gray-900">
+     R$ ${formatarPreco(p.precoParcelado.valorTotal)} em ${p.precoParcelado.parcelas}x de R$ ${p.precoParcelado.valorParcela} ${p.precoParcelado.descricao}
+    </span>
+  </div>
+
+
+  <div class="price-pix">
+     <span>ou <strong class="text-[1rem]">R$ ${formatarPreco(p.precoPix)}</strong> no Pix</span>
+  </div>
+
+  <div class= "bg-[#cc4419] rounded-lg cursor-pointer">
+    <a class="text-white py-[8px] font-bold">
+     Comprar</a>
+  </div>
+        </div>
+ </div>
+
+  `
+
+});
+const tabletsContainer = document.getElementById('tablets')
+tablets.forEach((t) => {
+   tabletsContainer.innerHTML +=`
+     <div class="card flex text-center">
+  <div class="img-content relative">
+      <div class="img w-[240px]">
+       <img src="${t.imagem}" alt="${t.alt}"/>
+      </div>
+
+      <div class="desconto absolute top-[0]">
+       <span class="bg-[#cc4410] text-white rounded-lg py-[3px] px-[5px] ml-[2px] font-bold text-[0.9rem]">
+        ${t.desconto}
+       </span>
+      </div>
+
+     <div class="title font-bold ">
+      <h1 class="mt-[5px]">${t.titulo}</h1>
+     </div>
+
+    <div class="memoria">
+     <p class="text-[1]">${t.memoria}</p>
+   </div>
+
+ <div class="price">  
+    <span class="text-[0.8rem] text-gray-900">
+     R$ ${formatarPreco(t.precoParcelado.valorTotal)} em ${t.precoParcelado.parcelas}x de R$ ${t.precoParcelado.valorParcela} ${t.precoParcelado.descricao}
+    </span>
+  </div>
+
+
+  <div class="price-pix">
+     <span>ou <strong class="text-[1rem]">R$ ${t.precoPix}</strong> no Pix</span>
+  </div>
+
+  <div class="bg-[#cc4419] rounded-lg cursor-pointer py-[8px] flex items-center justify-center">
+     <a class="text-white font-bold">
+     Comprar</a>
+  </div>
+        </div>
+ </div>
+
+   `
+});
+
+
